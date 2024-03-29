400b1d416ee0f570a75b73025a9e339f198dd747
sakurasou3 <ko.ki.0216@gmail.com>
2024-03-29 09:11:02 +0900

    Initialized application.rb

--- config/application.rb
+++ config/application.rb
@@ -32,6 +32,21 @@ module App
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
 
+    # Railsアプリのタイムゾーン(default 'UTC')
+    # Time.currentで取得できる日付を日本時間に設定する
+    # TimeZoneList: https://api.rubyonrails.org/classes/ActiveSupport/TimeZone.html
+    config.time_zone = ENV["TZ"]
+
+    # データベースの読み書きに使用するタイムゾーン(:local | :utc(default))
+    config.active_record.default_timezone = :utc
+
+    # i18nで使われるデフォルトのロケールファイルの指定(default :en)
+    config.i18n.default_locale = :ja
+
+    # $LOAD_PATHにautoload pathを追加しない(Aeitwerk有効時false推奨)
+    # load_path...$LOAD_PATHで表示されるパス一覧。requireで辿り着くGemのパスを定義している
+    config.add_autoload_paths_to_load_path = false
+
     # Only loads a smaller set of middleware suitable for API only apps.
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
